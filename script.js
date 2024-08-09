59ed2cf3182bcec7e6cf87155b0c5fb4cd87ed50
LiborSaja <sajalibor@gmail.com>
2024-08-09 11:36:05 +0200

    reset_added

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 //BS_AVION
-document.querySelector("#calculateButton").addEventListener("click", calculateTotalPrice);
-document.querySelector("#ticketForm").addEventListener("submit", function(e) {
+document
+    .querySelector("#calculateButton")
+    .addEventListener("click", calculateTotalPrice);
+document.querySelector("#ticketForm").addEventListener("submit", function (e) {
     e.preventDefault();
-    alert("Vaše objednávka byla přijata ke zpracování. O vyřízení Vás budeme informovat. Děkujeme, že využíváte našich služeb.");
+    alert(
+        "Vaše objednávka byla přijata ke zpracování. O vyřízení Vás budeme informovat. Děkujeme, že využíváte našich služeb."
+    );
     this.reset();
-    document.querySelector('#submitButton').disabled = true;
+    document.querySelector("#submitButton").disabled = true;
+    document.querySelector('#totalPrice').textContent = "";
 });
 
-document.querySelector("#note").addEventListener("input", function() {
+document.querySelector("#note").addEventListener("input", function () {
     // Povolené znaky: písmena a číslice -> pomocí regulárního výrazu
-    this.value = this.value.replace(/[^a-zA-Z0-9 áčďéěíňóřšťúůýžÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ.,]/g, '');
+    this.value = this.value.replace(
+        /[^a-zA-Z0-9 áčďéěíňóřšťúůýžÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ.,]/g,
+        ""
+    );
 });
 
 function calculateTotalPrice() {
     // Získání ceny letenky a počtu letenek
-    const pricePerTicket = parseInt(document.querySelector("#destination").value, 10);
-    const ticketCount = parseInt(document.querySelector("#ticketCount").value, 10);
+    const pricePerTicket = parseInt(
+        document.querySelector("#destination").value,
+        10
+    );
+    const ticketCount = parseInt(
+        document.querySelector("#ticketCount").value,
+        10
+    );
 
     // Výpočet základní ceny
     let totalPrice = pricePerTicket * ticketCount;
@@ -26,19 +40,27 @@ function calculateTotalPrice() {
     }
 
     // Přirážka za třídu
-    const classMultiplier = parseFloat(document.querySelector(".class:checked").value);
+    const classMultiplier = parseFloat(
+        document.querySelector(".class:checked").value
+    );
     totalPrice *= classMultiplier;
 
     // Zobrazení celkové ceny
-    document.querySelector("#totalPrice").value = `Celková cena: ${totalPrice} Kč`;
+    document.querySelector(
+        "#totalPrice"
+    ).value = `Celková cena: ${totalPrice} Kč`;
 
     // Kontrola rozpočtu
     const budget = parseInt(document.querySelector("#budget").value, 10);
     if (budget >= totalPrice) {
-        document.querySelector('#totalPrice').textContent = `${totalPrice} Kč. Nyní můžete přejít k závaznému objednání`;
+        document.querySelector(
+            "#totalPrice"
+        ).textContent = `${totalPrice} Kč. Nyní můžete přejít k závaznému objednání`;
         document.querySelector("#submitButton").disabled = false;
     } else {
-        document.querySelector('#totalPrice').textContent = `${totalPrice} Kč. Váš zadaný rozpočet je nedostatečný. Vaši objednávku nelze zpracovat.`;
+        document.querySelector(
+            "#totalPrice"
+        ).textContent = `${totalPrice} Kč. Váš zadaný rozpočet je nedostatečný. Vaši objednávku nelze zpracovat.`;
         document.querySelector("#submitButton").disabled = true;
     }
 }
